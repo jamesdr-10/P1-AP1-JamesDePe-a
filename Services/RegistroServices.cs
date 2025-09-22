c980acc099241e4c946077f560c112baa81a71b2
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-22 19:53:32 -0400

    Services creado

---
+++ Services/RegistroServices.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_JamesDePeña.DAL;
+using P1_AP1_JamesDePeña.Models;
+using System.Linq.Expressions;
+
+namespace P1_AP1_JamesDePeña.Services;
+
+public class RegistroServices(IDbContextFactory<Contexto> DbFactory)
+{
+    public async Task<List<Registro>> Listar(Expression<Func<Registro, bool>> criterio)
+    {
+        await using var contexto = await DbFactory.CreateDbContextAsync();
+        return await contexto.Registro.Where(criterio).AsNoTracking().ToListAsync();
+    }
+}
