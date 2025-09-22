1822de6dd385c0c402c12d7ca030d6e8820e67f9
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-22 19:25:15 -0400

    Contexto creado

---
+++ DAL/Contexto.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using P1_AP1_JamesDePeña.Models;
+
+namespace P1_AP1_JamesDePeña.DAL;
+
+public class Contexto : DbContext
+{
+    public DbSet<Registro> Registro { get; set; }
+
+    public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+}
