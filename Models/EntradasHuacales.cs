60d01203757309d426dc167ea308e8c47d8998ef
jamesdr-10 <james_depena@ucne.edu.do>
2025-09-29 18:42:21 -0400

    Completando modelo EntradasHuacales

--- Models/EntradasHuacales.cs
+++ Models/EntradasHuacales.cs
@@ -5,6 +5,18 @@ namespace P1_AP1_JamesDePeña.Models;
 public class EntradasHuacales
 {
     [Key]
-    public int RegistroId { get; set; }
+    public int IdEntrada { get; set; }
 
+    [Required(ErrorMessage = "La fecha es obligatoria.")]
+    public DateTime Fecha { get; set; } = DateTime.Now;
+
+    [Required(ErrorMessage = "El nombre es obligatorio.")]
+    [RegularExpression(@"^[a-zA-Z\s]+$", ErrorMessage = "El nombre solo puede tener letras y espacios.")]
+    public string NombreCliente { get; set; } = string.Empty;
+
+    [Range(1, int.MaxValue, ErrorMessage = "La cantidad debe ser mayor que 0.")]
+    public int Cantidad { get; set; }
+
+    [Range(1, double.MaxValue, ErrorMessage = "El precio debe ser mayor que 0.")]
+    public double Precio { get; set; }
 }
