d652134e47726982374b7cbb6bce4c90a9cac20f
jamesdr-10 <james_depena@ucne.edu.do>
2025-10-07 12:59:40 -0400

    Agregando modelo TiposHuacales

---
+++ Models/TiposHuacales.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JamesDePeña.Models;
+
+public class TiposHuacales
+{
+    [Key]
+    public int TipoId { get; set; }
+
+    [Required(ErrorMessage = "La descripción es obligatoria.")]
+    public string Descripcion { get; set; } = string.Empty;
+
+    [Range(0, int.MaxValue, ErrorMessage = "La existencia debe ser igual o mayor que 0.")]
+    public int Existencia { get; set; }
+
+    [InverseProperty("TipoHuacal")]
+    public virtual ICollection<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; } = new List<EntradasHuacalesDetalle>();
+}
