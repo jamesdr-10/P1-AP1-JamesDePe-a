8e588292758f428bf8c70957567f4ea0b3ff4aba
jamesdr-10 <james_depena@ucne.edu.do>
2025-10-07 12:59:57 -0400

    Agregando modelo EntradasHuacalesDetalle

---
+++ Models/EntradasHuacalesDetalle.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace P1_AP1_JamesDePeña.Models;
+
+public class EntradasHuacalesDetalle
+{
+    [Key]
+    public int DetalleId { get; set; }
+
+    public int IdEntrada { get; set; }
+
+    public int TipoId { get; set; }
+
+    public int Cantidad { get; set; }
+
+    public double Precio { get; set; }
+
+    [ForeignKey("IdEntrada")]
+    [InverseProperty("EntradasHuacalesDetalle")]
+    public virtual EntradasHuacales EntradaHuacal { get; set; }
+
+    [ForeignKey("TipoId")]
+    [InverseProperty("EntradasHuacalesDetalle")]
+    public virtual TiposHuacales TipoHuacal { get; set; }
+}
