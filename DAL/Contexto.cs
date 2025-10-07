7e3239119fd3eb15bbf7b0dd120055f30f612829
jamesdr-10 <james_depena@ucne.edu.do>
2025-10-07 13:00:26 -0400

    Modificando contexto y agregando OnModelCreate

--- DAL/Contexto.cs
+++ DAL/Contexto.cs
@@ -6,6 +6,17 @@ namespace P1_AP1_JamesDePeña.DAL;
 public class Contexto : DbContext
 {
     public DbSet<EntradasHuacales> EntradasHuacales { get; set; }
+    public DbSet<EntradasHuacalesDetalle> EntradasHuacalesDetalle { get; set; }
+    public DbSet<TiposHuacales> TiposHuacales { get; set; }
 
     public Contexto(DbContextOptions<Contexto> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        modelBuilder.Entity<TiposHuacales>().HasData(
+            new TiposHuacales { TipoId = 1, Descripcion = "Huacal Rojo", Existencia = 50 },
+            new TiposHuacales { TipoId = 2, Descripcion = "Huacal Verde", Existencia = 10 }
+            );
+    }
 }
